7a8747bbf50d2ac44070623e8cc173eed5707270
chirkov.e <chirkov.e@medlinx.online>
2021-10-22 19:53:12 +0300

    rename merchandise-service to MerchandiseService

--- src/MerchandiseService/Program.cs
+++ src/MerchandiseService/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace merchandise_service
+namespace MerchandiseService
 {
     public class Program
     {
--- src/MerchandiseService/Startup.cs
+++ src/MerchandiseService/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace merchandise_service
+namespace MerchandiseService
 {
     public class Startup
     {
